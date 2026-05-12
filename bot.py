99e4a3d6fd36fd3d3281f7875e0c5f0d503096f1
mavejpeg <91692420+mavejpeg@users.noreply.github.com>
2026-05-12 21:18:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from handlers.post import post_conversation
 from handlers.admin import handlers as admin_handlers
 from handlers.queue import queue_handler
 from handlers.start import start
+from telegram.ext import MessageHandler, filters
+from handlers.queue import show_queue
 
 nest_asyncio.apply()
 logging.basicConfig(level=logging.INFO)
@@ -25,6 +27,7 @@ async def main():
     for h in admin_handlers:
         app.add_handler(h)
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Regex("^📋 Очередь$"), show_queue))
 
     logger.info("Бот запущен через polling")
     await app.run_polling(drop_pending_updates=True)
